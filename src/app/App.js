d7c0a2f96d52803259dadae719ab4a8c38ec4fbd
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-03-12 12:38:57 +0200

    solve yarn test

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,16 @@
 import React, { Suspense  } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import currencyReducer from './reducers/currencyReducer';
 import styles from './App.scss';
 import Header from './components/Header/Header';
 import ExchangeRates from './components/ExchangeRates/ExchangeRates';
 
+const store = createStore(currencyReducer);
 const ATMs = React.lazy(() => import('./components/ATMs/ATMs'));
 const AtmsPage = () => <ATMs />;
-const ExchangeRatesPage = () => <ExchangeRates />;
+const ExchangeRatesPage = () => <Provider store={store}><ExchangeRates /></Provider>;
 
 const App = () => {
   return (
