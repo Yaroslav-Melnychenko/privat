7755987944dc0ab307eac65ab7563eca30801200
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-03-19 16:23:40 +0200

    move store logic to Store folder

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from 'Store/reducers';
+import { store } from 'Store/configureStore';
 import Routes from './Routes';
 import Header from './Header';
 
 require('dotenv').config();
-
-// import styles from './App.scss';
-
-const store = createStore(reducers, applyMiddleware(thunk));
 
 const App = () => {
   return (
