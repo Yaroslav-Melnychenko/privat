31a4a218b48edcc0f90665cb7f4d48e5b86c335a
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-03-20 16:56:58 +0200

    add news search

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,9 +13,26 @@ const Home = () => {
       });
   }, []);
 
+  const findNews = (e) => {
+    const text = e.target.value;
+    if (text !== '') {
+      axios.get(`https://newsapi.org/v2/everything?q=${text}&from=&sortBy=popularity&apiKey=357f3d3ae7304b9da671fa6f5e934d60`)
+      .then(news => {
+        setPosts(news.data.articles);
+      });
+    } else {
+      axios.get('https://newsapi.org/v2/top-headlines?country=ru&apiKey=357f3d3ae7304b9da671fa6f5e934d60')
+      .then(news => {
+        setPosts(news.data.articles);
+      });
+    }
+  }
+
   return (
     <div className={styles.container}>
       <h1>Последние новости</h1>
+      <span>Найти новость: </span> 
+      <input type="text" onChange={findNews} />
       {
         posts.map(post => (
           <div key={Math.random()} className={styles.newsContainer}>
